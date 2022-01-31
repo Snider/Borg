45e9024786ef21c774759ff337be1527a0d92b7d
Snider <snider@lethean.io>
2022-01-31 15:48:34 +0000

    Dev Git Clone Tool: `npm run start letheanVPN`

--- README.md
+++ README.md
@@ -6,3 +6,31 @@ WIP; process will be as follows:
 - [] Apply Branding changes
 - [] Push to new repo on`updtream`, --depth=1
 - [] Un-Assimiulate Backport to `upstream` format
+
+
+## Usage
+
+### Show ENV Variables
+
+```shell
+# Linux/macOS
+npm run env-linux
+
+# Windows
+npm run env-windows
+```
+
+### Run 
+
+```shell
+# Linux/macOS
+npm run start-linux
+
+# Windows
+npm run start-windows
+```
+
+### Pull GitHub User
+```shell
+npm run start letheanVPN
+```
--- tractor-beam.js
+++ tractor-beam.js
@@ -1,64 +1,53 @@
-"use strict";
-const fs = require('fs')
-const fse = require('fs-extra');
-const path = require('path');
+#!/usr/bin/env node
+
+const rm = require('rimraf').sync;
 const download = require('download-git-repo');
-const rm = require('rimraf').sync
-let root = 'src/assets/content'
+const { Octokit } = require("@octokit/core");
 
-let repos = [
-    {project: 'chain', name: 'blockchain', url: 'https://gitlab.com/lthn.io/projects/chain/lethean.git#next'},
-    {project: 'vpn', name: 'vpn', url: 'https://gitlab.com/lthn.io/projects/vpn/node.git'},
-    {project: 'build', name: 'build', url: 'https://gitlab.com/lthn.io/projects/sdk/build.git#main'}
-]
 
-function parseDir(base) {
-    let tree = []
-    let meta = {}
-    let dirs = fs.readdirSync(base)
-    dirs.forEach((dir) => {
-        if (fs.statSync(path.join(base, dir)).isDirectory()) {
-            let dirUrl = path.join(base.replace(root, ''), dir)
-            let docUrl = path.join(base.replace(root, ''), dirUrl.replace(dirUrl.substring(0, 4), '') + '/index.md')
-            let httpUrl = dirUrl.replace(dirUrl.substring(0, 4), '')
-            let children = parseDir(path.join(base, dir))
-            meta[httpUrl.toString()] = dirUrl + '/index.md'
-            tree.push({
-                name: dir,
-                url: httpUrl,
-                doc: docUrl,
-                children: children.tree
+
+// Create a personal access token at https://github.com/settings/tokens/new?scopes=repo
+
+const octokit = new Octokit({ auth: process.env.GH_TOKEN});
+const target = process.env.BORG_TARGET ?  process.env.BORG_TARGET : process.argv[2] ? process.argv[2] : 'letheanVPN'
+console.log(`Scanning For: ${target}`)
+
+const MyActionOctokit = Octokit.plugin(
+    require("@octokit/plugin-paginate-rest").paginateRest,
+    require("@octokit/plugin-throttling").throttling,
+    require("@octokit/plugin-retry").retry
+).defaults({
+    throttle: {
+        onAbuseLimit: (retryAfter, options) => {
+            /* ... */
+        },
+        onRateLimit: (retryAfter, options) => {
+            /* ... */
+        },
+    },
+    authStrategy: require("@octokit/auth-action").createActionAuth,
+    userAgent: `my-octokit-action/v1.2.3`,
+});
+
+async function getList() {
+    return await octokit.request(`GET /users/${target}/repos`, {
+        type: "public",
+
+    }).then((repos) => {
+        repos.data.forEach((repo) => {
+            console.log(`Brig Clean: ${repo.full_name}`)
+            rm(`brig/${repo.full_name}`)
+            download(`${repo.full_name}`, `brig/${repo.full_name}`, {
+                // clone: true, depth: 1
+            }, function (err) {
+                if (err) throw err
+                console.log(`Assimilated ${repo.full_name} Repository`)
             })
-            for (const [key, value] of Object.entries(children.meta)) {
-                meta[key.toString()] = value
-            }
-        } else {
-            if (dir !== 'index.md') {
-                let fileUrl = path.join(base.replace(root, ''), dir.replace('.md', ''))
-                let fileDoc = path.join(base.replace(root, ''), dir)
-                let httpUrl = fileUrl.replace(fileUrl.substring(0, 4), '')
-                tree.push({
-                    name: dir.replace('.md', ''),
-                    doc: fileDoc,
-                    url: httpUrl
-                })
-                meta[httpUrl] = fileDoc
-            }
-        }
-    })
-    return { meta: meta, tree: tree }
+        })
+    });
 }
 
-console.log("Staring async Doc build")
 
-repos.forEach((repo) => {
-    rm(`docs/${repo.project}`)
-    console.log(`Cloning ${repo.name} Project`)
-    download(`direct:${repo.url}`, `brig/${repo.project}`, {
-        clone: true, depth: 1
-    }, function (err) {
-        if (err) throw err
-        console.log(`Assimilated ${repo.name} Repository`)
+console.log(`We are the Borg. Your technological distinctiveness will be added to our own & contributed too`)
 
-    })
-})
+module.exports = getList();
